4370909bed83c9567955e3e4d9e4713bfd3d589e
Scott Sutherland <scott@webcheddar.ca>
2024-03-21 22:14:35 -0400

    Kinsta debugging

--- next.config.js
+++ next.config.js
@@ -1,24 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
-
-module.exports = {
+const nextConfig = {
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
   },
   images: {
-    domains: ['shop.webcheddar.ca', 'webcheddar.ca'], // List of domains from where images can be loaded
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840], // Different screen sizes
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384], // Widths for different image sizes
-    path: '/_next/image', // The path where to load the images from
-    loader: 'default', // The loader to use for the images, use 'default' unless you have a custom image loader
+    domains: ['shop.webcheddar.ca', 'webcheddar.ca'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    path: '/_next/image',
+    loader: 'default',
   },
-  trailingSlash: true,  
+  trailingSlash: true,
   // Add other configurations here as needed
 };
 
-// module.exports = {
-  
-// }
+module.exports = nextConfig;
