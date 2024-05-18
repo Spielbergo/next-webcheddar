32a517f7ff91849ecd0b45ec1660b249bbe4eb99
Scott Sutherland <scott@webcheddar.ca>
2024-05-18 00:23:22 -0400

    Added marquee effect to client logos on mobile

--- components/Clients.component.jsx
+++ components/Clients.component.jsx
@@ -1,26 +1,40 @@
 import Image from 'next/image';
 import styles from '../styles/clients.module.css';
-
 import { clientLogos } from '../constants/index';
 
- const Clients = () => {
+const Clients = () => {
     return (
         // Clients Section
-        <section>
-            <ul className={styles.client_logos__container}>
-            {clientLogos.map((client) => 
-                <li key={client.id} className={styles.client_logos__logo}>
-                    <Image  
-                        src={client.image}
-                        alt={client.alt}
-                        width={125}
-                        height={50}
-                    />
-                </li>
-            )}
-            </ul>
+        <section className={styles.client_section}>
+            <div className={styles.client_logos__marquee}>
+                <ul className={styles.client_logos__container}>
+                    {clientLogos.map((client) =>
+                        <li key={client.id} className={styles.client_logos__logo}>
+                            <Image
+                                src={client.image}
+                                alt={client.alt}
+                                width={125}
+                                height={50}
+                            />
+                        </li>
+                    )}
+                </ul>
+                {/* Duplicate the logos for the infinite loop effect only on mobile */}
+                <ul className={`${styles.client_logos__container} ${styles.client_logos__container_duplicate}`}>
+                    {clientLogos.map((client) =>
+                        <li key={`${client.id}-duplicate`} className={styles.client_logos__logo}>
+                            <Image
+                                src={client.image}
+                                alt={client.alt}
+                                width={125}
+                                height={50}
+                            />
+                        </li>
+                    )}
+                </ul>
+            </div>
         </section>
-    )
-}
+    );
+};
 
 export default Clients;
