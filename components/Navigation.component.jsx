ee5763ad68728399ba21896d12b9db9fcea4b307
Scott Sutherland <scott@webcheddar.ca>
2024-07-29 23:26:47 -0400

    Nav menu update

--- components/Navigation.component.jsx
+++ components/Navigation.component.jsx
@@ -3,25 +3,27 @@ import { useRouter } from 'next/router';
 import Image from 'next/image';
 import Link from 'next/link';
 import { FaEnvelope } from 'react-icons/fa';
-import { navigation, socialIcons } from '../data'; // Importing navigation and socialIcons data
-import services from '../data/services'; // Importing services data
+// import { IoChevronBackCircleOutline } from "react-icons/io5";
+import { BsXCircle } from "react-icons/bs";
+import { navigation, socialIcons } from '../data';
+import services from '../data/services';
 import styles from './navigation.module.css';
 import Socials from '../components/SocialIcons.component';
 import NavLogoWhite from '../public/logos/logo-light.png';
 import NavLogoBlack from '../public/logos/logo-dark.png';
-
 import ContactForm from './ContactForm.component';
 import Button from './Button.component';
-import Modal from './Modal.component'; // Import the Modal component
-import { useModal } from '../contexts/ModalContext'; // Import the modal context
+import Modal from './Modal.component';
+import { useModal } from '../contexts/ModalContext';
 
 const Navigation = () => {
   const [scrolled, setScrolled] = useState(false);
   const [mobileToggleOpen, setMobileToggleOpen] = useState(false);
   const [subLinkToggleOpen, setSubLinkToggleOpen] = useState(false);
+  const [activeSubmenu, setActiveSubmenu] = useState(null);
   const navRef = useRef();
   const router = useRouter();
-  const { openModal } = useModal(); // Get the openModal function from context
+  const { openModal } = useModal();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -32,6 +34,7 @@ const Navigation = () => {
       if (navRef.current && !navRef.current.contains(event.target) && mobileToggleOpen) {
         setMobileToggleOpen(false);
         setSubLinkToggleOpen(false);
+        setActiveSubmenu(null);
       }
     };
 
@@ -46,35 +49,33 @@ const Navigation = () => {
 
   const handleMobileToggle = () => {
     setMobileToggleOpen(prevState => !prevState);
-    if (subLinkToggleOpen) setSubLinkToggleOpen(false); // Close submenu if open
+    if (subLinkToggleOpen) setSubLinkToggleOpen(false);
+    setActiveSubmenu(null);
   };
 
-  const handleSubLinkToggle = () => {
+  const handleSubLinkToggle = (submenuId) => {
     setSubLinkToggleOpen(prevState => !prevState);
+    setActiveSubmenu(submenuId);
   };
 
   const handleCloseMobileNav = () => {
     setMobileToggleOpen(false);
     setSubLinkToggleOpen(false);
+    setActiveSubmenu(null);
   };
 
-  // Clone the navigation array to avoid modifying the original data
+  const handleBackButtonClick = () => {
+    setSubLinkToggleOpen(false);
+    setActiveSubmenu(null);
+  };
+
   const navItems = JSON.parse(JSON.stringify(navigation));
-
-  // Find the services item in the navigation array
   const servicesNavItem = navItems.find(item => item.id === 'services');
-
-  // Populate subLinks dynamically from services
   servicesNavItem.subLinks = services.map(service => ({
     id: service.slug,
     anchor: service.title,
-    link: `/services/${service.slug}`, // Ensure the link points to the correct path
+    link: `/services/${service.slug}`,
   }));
-
-  const handleSubLinkClick = () => {
-    setMobileToggleOpen(false); // Close the mobile menu
-    setSubLinkToggleOpen(false); // Close the submenu when a submenu item is clicked
-  };
 
   return (
     <nav ref={navRef} className={`${styles.main_nav__nav} ${scrolled ? styles.main_nav__scrolled : ''} ${mobileToggleOpen ? styles.main_nav__container_grey : ''}`}>
@@ -96,29 +97,29 @@ const Navigation = () => {
         <div className={styles.main_nav__links_container}>
           <div>
             <ul className={`${styles.main_nav__navLinks} ${mobileToggleOpen ? styles.main_nav__navLinks_visible : ''}`}>
-              {navItems.map((nav) => (
-                <li key={nav.id} className={styles.main_nav__subLinks__parent}
-                    onMouseEnter={nav.id === 'services' ? () => setSubLinkToggleOpen(true) : null}
-                    onMouseLeave={nav.id === 'services' ? () => setSubLinkToggleOpen(false) : null}
-                    onClick={nav.id === 'services' ? handleSubLinkToggle : handleCloseMobileNav}>
-                  {nav.id === 'services' ? (
-                    <span className={styles.nav_item}>{nav.anchor}</span>
-                  ) : (
-                    <Link href={nav.link} className={router.pathname === nav.link ? styles.active : ''} onClick={handleCloseMobileNav}>{nav.anchor}</Link>
-                  )}
-                  {nav.subLinks && nav.subLinks.length > 0 && (
-                    <ul className={`${styles.main_nav__subLinks} shadow ${subLinkToggleOpen ? styles.main_nav__sublinks_visible : ''}`}>
-                      {nav.subLinks.map(subLink => (
-                        <li key={subLink.id} onClick={handleSubLinkClick}>
-                          <Link href={subLink.link} className={router.pathname === subLink.link ? styles.active : ''}>
-                            <span data-hover={subLink.anchor}>{subLink.anchor}</span>
-                          </Link>
-                        </li>
-                      ))}
-                    </ul>
-                  )}
-                </li>
-              ))}
+              {!subLinkToggleOpen ? (
+                navItems.map((nav) => (
+                  <li key={nav.id} className={styles.main_nav__subLinks__parent}
+                      onClick={nav.id === 'services' ? () => handleSubLinkToggle(nav.id) : handleCloseMobileNav}>
+                    {nav.id === 'services' ? (
+                      <span className={styles.nav_item}>{nav.anchor}</span>
+                    ) : (
+                      <Link href={nav.link} className={router.pathname === nav.link ? styles.active : ''} onClick={handleCloseMobileNav}>{nav.anchor}</Link>
+                    )}
+                  </li>
+                ))
+              ) : (
+                <>
+                  <li className={styles.main_nav__back_button} onClick={handleBackButtonClick}><BsXCircle /><span>Back</span></li>
+                  {servicesNavItem.subLinks.map(subLink => (
+                    <li key={subLink.id} onClick={handleCloseMobileNav}>
+                      <Link href={subLink.link} className={router.pathname === subLink.link ? styles.active : ''}>
+                        <span data-hover={subLink.anchor}>{subLink.anchor}</span>
+                      </Link>
+                    </li>
+                  ))}
+                </>
+              )}
               <span className={styles.main_nav__mobile_links}>
                 <li><Socials /></li>
               </span>
